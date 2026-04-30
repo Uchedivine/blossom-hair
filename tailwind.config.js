f2edc54c2b10aa859895a3f2bb26fb445624e95a
Uchedivine <uchedivine65@gmail.com>
2026-04-30 17:33:44 +0100

    Add pink theme and mobile responsiveness
    - Changed primary color from amber to rose/pink
    - Added Blossom Hair branding with custom logo
    - Enabled dark mode with theme switcher
    - Added mobile-responsive optimizations
    - Improved touch targets for mobile
    - Added custom pink scrollbar
    - Enabled SPA mode for faster navigation

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import preset from './vendor/filament/support/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    50: '#fdf2f8',
+                    100: '#fce7f3',
+                    200: '#fbcfe8',
+                    300: '#f9a8d4',
+                    400: '#f472b6',
+                    500: '#ec4899',
+                    600: '#db2777',
+                    700: '#be185d',
+                    800: '#9f1239',
+                    900: '#831843',
+                    950: '#500724',
+                },
+            },
+        },
+    },
+    plugins: [],
+}
